5ec3ba6093b8757c08eb408970ba25a87f881c3c
Maria Gamal <maria.gamal13@gmail.com>
2021-07-13 01:20:27 +0200

    Added project skeleton

---
+++ main.py
@@ -0,0 +1,20 @@
+class Sudoko:
+    def __init__(self):
+        self.board = None # Random numbers initialization Use opencv or prettytable or strings
+        self.difficulty = None # User choses game difficulty
+
+    def newMove(self):
+        '''
+        Takes input number from user
+        checks for validation
+        exits when complete
+        :return:
+        '''
+        pass
+
+    def printBoard(self):
+        '''
+
+        :return:
+        Updated board every loop
+        '''
